22a5d9f8c62480ab0a442b24b8be5f1adadcce7b
David Davies <d.davies90@hotmail.co.uk>
2021-09-09 11:58:29 +0100

    Added remaining form inputs and the state associated with them.

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,20 +1,33 @@
 import { useState } from 'react';
+import axios from 'axios';
 import '../styles/AddProperty.scss';
 
 const AddProperty = () => {
   const initialState = {
     fields: {
+      bathrooms: '',
+      bedrooms: '',
+      city: '',
+      email: '',
+      price: '',
       title: '',
-      city: 'Manchester'
+      type: ''
     }
   };
 
-  // eslint-disable-next-line no-unused-vars
   const [fields, setFields] = useState(initialState.fields);
 
-  const handleAddProperty = (event) => {
+  const handleAddProperty = async (event) => {
+    const endpoint = 'http://localhost:4000/api/v1/PropertyListing';
     event.preventDefault();
-    console.log(fields);
+    try {
+      const response = await axios.post(endpoint, fields);
+      if (response.status === 201) {
+        console.log(response.statusText);
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const handleFieldChange = (event) => {
@@ -31,6 +44,20 @@ const AddProperty = () => {
             name="title"
             value={fields.title}
             onChange={handleFieldChange}
+            placeholder="Enter a property name"
+            required
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+            placeholder="Email"
+            required
           />
         </label>
         <label htmlFor="city">
@@ -40,14 +67,77 @@ const AddProperty = () => {
             name="city"
             value={fields.city}
             onChange={handleFieldChange}
+            required
           >
-            <option defaultValue="Manchester" value="Manchester">
-              Manchester
+            <option value="" defaultValue disabled>
+              Select City
             </option>
+            <option value="Manchester">Manchester</option>
             <option value="Leeds">Leeds</option>
             <option value="Sheffield">Sheffield</option>
             <option value="Liverpool">Liverpool</option>
           </select>
+        </label>
+        <label htmlFor="type">
+          Type
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+            required
+          >
+            <option value="" defaultValue disabled>
+              Select property type
+            </option>
+            <option value="flat">Flat</option>
+            <option value="detatched">Detatched</option>
+            <option value="semi-detatched">Semi-Detatched</option>
+            <option value="terraced">Terraced</option>
+            <option value="end-of-terrace">End-of-Terrace</option>
+            <option value="cottage">Cottage</option>
+            <option value="bungalow">Bungalow</option>
+          </select>
+        </label>
+        <label htmlFor="bedrooms">
+          Bedrooms
+          <input
+            id="bedrooms"
+            name="bedrooms"
+            type="number"
+            min="1"
+            value={fields.bedrooms}
+            onChange={handleFieldChange}
+            placeholder="No. of bedrooms"
+            required
+          />
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms
+          <input
+            id="bathrooms"
+            name="bathrooms"
+            type="number"
+            min="1"
+            value={fields.bathrooms}
+            onChange={handleFieldChange}
+            placeholder="No. of bathrooms"
+            required
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            id="price"
+            name="price"
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={fields.price}
+            onChange={handleFieldChange}
+            placeholder="Price (£)"
+            required
+          />
         </label>
         <button type="submit">Add</button>
       </form>
